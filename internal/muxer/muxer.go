507a257506bb7e8fc7951bf18636b1044d8919a7
lifthus <lifthus531@gmail.com>
2023-10-22 23:11:12 +0900

    make doker

--- internal/muxer/muxer.go
+++ internal/muxer/muxer.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func SetGockerApi(mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("This is the Gocker server"))
+		w.Write([]byte("This is the Gocker server!"))
 	})
 	return mux
 }
