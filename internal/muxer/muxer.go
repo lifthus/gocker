afca358c5888f87ffb3f2d086c661a9cb42d2d34
lifthus <lifthus531@gmail.com>
2023-10-24 15:26:35 +0900

    docker compose

--- internal/muxer/muxer.go
+++ internal/muxer/muxer.go
@@ -1,10 +1,13 @@
 package muxer
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func SetGockerApi(mux *http.ServeMux) *http.ServeMux {
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("This is the Gocker server!"))
+		w.Write([]byte("This is the Gocker server!" + os.Getenv("ENV1")))
 	})
 	return mux
 }
